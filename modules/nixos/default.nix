1385c58762723480f6344c5eb525b43d260d5511
Kai <kai.couts@gmail.com>
2023-11-16 16:11:25 +0900

    Add acpi

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -138,6 +138,8 @@ in
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    acpi
+    acpica-tools
     brightnessctl
     dunst
     gcc
